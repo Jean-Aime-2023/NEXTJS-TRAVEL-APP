bf9e8f339d184bb801f5dabaf878956cc5e0cac8
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-10-14 21:57:10 +0200

    Animation added and ui finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+
+
 import Navbar from '@/components/Navbar';
 import './globals.css';
 import type { Metadata } from 'next';
 import Footer from '@/components/Footer';
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Travel',
   description: 'Travel UI/UX App for Camping',
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,30 @@
+"use client"
+
 import { NAV_LINKS } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import Button from './Button';
+import Aos from 'aos';
+import 'aos/dist/aos.css'
+import { useEffect } from 'react';
 
 const Navbar = () => {
+  useEffect(()=>{
+    Aos.init({offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,});
+  },[])
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
+      <Link href="/" data-aos="fade-up">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
+          data-aos="fade-down" 
             href={link.href}
             key={link.key}
             className="regular-16 text-gray-50 flexCenter cursor pointer pb-1.5 transition-all hover:font-bold"
@@ -23,10 +35,10 @@ const Navbar = () => {
       </ul>
 
       <div className='lg:flexCenter hidden'>
-        <Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green"/>
+        <div data-aos="fade-down"><Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green"/></div>
       </div>
 
-      <Image src="menu.svg" alt='menu' width={32} height={32} className='inline-block cursor-pointer lg:hidden' />
+      <Image src="menu.svg" alt='menu' width={32} height={32} className='inline-block cursor-pointer lg:hidden' data-aos="fade-right"/>
     </nav>
   );
 };
