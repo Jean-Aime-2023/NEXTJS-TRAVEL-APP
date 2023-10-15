3a6544db179fcf66a64aef81d19c81a6a9d1fcc6
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-10-15 14:09:33 +0200

    Final Commits

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -65,9 +65,31 @@ const Camp = () => {
           easing: 'ease-in-sine',
           delay: 100,});
       },[])
+
+      const scrollLeft = () => {
+        const content = document.getElementById("content");
+        if (content) {
+          const currentScrollLeft = content.scrollLeft;
+          content.scrollLeft = currentScrollLeft - 900;
+        }
+      };
+      
+      const scrollRight = () => {
+        const content = document.getElementById("content");
+        if (content) {
+          const currentScrollLeft = content.scrollLeft;
+          content.scrollLeft = currentScrollLeft + 900;
+        }
+      };
+      
+    
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        <div className="flex flex-row justify-end">
+            <div className="p-2 m-2 rounded-full cursor-pointer bg-gray-10 rotate-90" onClick={scrollLeft}><Image src="/uiw_down.png" alt="icon" height={20} width={20}/></div>
+            <div className="p-2 m-2 rounded-full cursor-pointer bg-gray-10 -rotate-90" onClick={scrollRight}><Image src="/uiw_down.png" alt="icon" height={20} width={20}/></div>
+        </div>
+      <div id="content" className="hide-scrollbar scroll-smooth flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite 
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,14 @@ import Link from 'next/link';
 import Button from './Button';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 
 const Navbar = () => {
+  const [nav,setNav] = useState(false)
+
+  const handleNav = ()=>{
+    setNav(!nav)
+  }
   useEffect(()=>{
     Aos.init({offset: 200,
       duration: 600,
@@ -16,6 +21,7 @@ const Navbar = () => {
       delay: 100,});
   },[])
   return (
+    <>
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/" data-aos="fade-up">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
@@ -38,8 +44,45 @@ const Navbar = () => {
         <div data-aos="fade-down"><Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green"/></div>
       </div>
 
-      <Image src="menu.svg" alt='menu' width={32} height={32} className='inline-block cursor-pointer lg:hidden' data-aos="fade-right"/>
+      <Image onClick={handleNav} src="menu.svg" alt='menu' width={32} height={32} className='inline-block cursor-pointer lg:hidden' data-aos="fade-right"/>
+
+      
     </nav>
+    <div className={nav ? ' z-50 fixed left-0 top-0 w-full h-screen bg-black/70':''}>
+        <div className={nav?"fixed left-0 top-0 w-[75%] sm:w-[45%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500":"fixed left-[-100%] p-10 top-0 ease-in duration-500"}>
+          <div>
+            <div className='flex w-full items-center justify-between'>
+            <Link href="/" data-aos="fade-up">
+              <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+            </Link>
+            <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-3 py-1 cursor-pointer'>
+              x
+            </div>
+            </div>
+            <div className='border-b border-gray-300 my-4'>
+              <p className='w-[85%] md:w-[90%] py-4'>Let's explore the world</p>
+            </div>
+          </div>
+          <div className='py-4'>
+            <ul className="capitalize flex flex-col gap-5 mt-4">
+              {NAV_LINKS.map((link) => (
+                <Link
+                data-aos="fade-down" 
+                  href={link.href}
+                  key={link.key}
+                  className="regular-16 text-gray-50 cursor pointer pb-1.5 transition-all hover:font-bold"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </ul>
+            <div className='mt-5 ml-[-10px]'>
+              <div data-aos="fade-down"><Button type="button" title="Login" icon="/user.svg" variant="btn_dark_green"/></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
